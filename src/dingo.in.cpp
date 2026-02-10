54316d7a75263171ce9bf37a02cd2200da568f07
chuoru <tranvietthanh0101@gmail.com>
2026-02-10 16:03:12 +0900

    feat(robot): modify rsdf and add floating base for mobile base

--- src/dingo.in.cpp
+++ src/dingo.in.cpp
@@ -18,10 +18,9 @@ namespace mc_robots {
 DingoRobotModule::DingoRobotModule()
     : mc_rbdyn::RobotModule(MC_DINGO_DESCRIPTION_PATH, "dingo") {
   // True if the robot has a fixed base, false otherwise
-  bool fixed = true;
+  bool fixed = false;
   // Makes all the basic initialization that can be done from an URDF file
   init(rbd::parsers::from_urdf_file(urdf_path, fixed));
-
   // Automatically load the convex hulls associated to each body
   std::string convexPath = path + "/convex/" + name + "/";
   bfs::path p(convexPath);
@@ -44,19 +43,7 @@ DingoRobotModule::DingoRobotModule()
   _commonSelfCollisions = _minimalSelfCollisions;
 
   // Ref joint order
-  _ref_joint_order = {"left_wheel", "right_wheel"};
-
-  // Stance: joint name, angle in degrees
-  std::map<std::string, double> starting{
-      {"left_wheel", 0.0},
-      {"right_wheel", 0.0},
-  };
-
-  for (const auto &j : mb.joints()) {
-    if (j.name() != "Root" && j.dof() > 0) {
-      _stance[j.name()] = {starting.at(j.name())};
-    }
-  }
+  _ref_joint_order = {};
 }
 
 } // namespace mc_robots
